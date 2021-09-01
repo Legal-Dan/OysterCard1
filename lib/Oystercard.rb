a983dd27520bf197b240354b2704cad710be01da
Daniel Clements <clemies2000@hotmail.com>
2021-09-01 16:04:58 +0100

    complete challenge 8

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -6,11 +6,28 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_use = false
   end
   
   def top_up(cash)
-    raise 'maximum exceeded: #{MAXIMUM_BALANCE}' if @balance + cash >= MAXIMUM_BALANCE
+    raise "maximum exceeded: #{MAXIMUM_BALANCE}" if @balance + cash >= MAXIMUM_BALANCE
     @balance += cash
   end 
+
+  def deduct(cash)
+    @balance -= cash
+  end 
+
+  def in_use?
+    @in_use
+  end
+
+  def touch_in
+    @in_use = true
+  end
+  
+  def touch_out
+    @in_use = false
+  end
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -14,4 +14,25 @@ describe Oystercard do
     expect{ (subject.top_up(100)) }.to raise_error("maximum exceeded: #{Oystercard::MAXIMUM_BALANCE}")
   end
 
+  it 'deducts money from the card' do
+    subject.top_up(2)
+    subject.deduct(1)
+    expect(subject.balance).to eq 1
+  end
+
+  it 'can identify if a card is currently in use on a journey' do
+    expect(subject).not_to be_in_use
+  end
+
+  it 'can touch in and mark the card as in use' do
+    subject.touch_in
+    expect(subject).to be_in_use
+  end
+
+  it 'can touch out and mark the card as no longer in use' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_use
+  end
+
 end 
